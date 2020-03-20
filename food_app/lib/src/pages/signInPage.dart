e4d42e185426522f678beeaa6b60875bc67846a9
guptabhoomika <43852276+guptabhoomika@users.noreply.github.com>
2020-03-20 14:51:07 +0530

    update

--- food_app/lib/src/pages/signInPage.dart
+++ food_app/lib/src/pages/signInPage.dart
@@ -49,105 +49,110 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Sign In",
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 100.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+      body: Center(
+        //padding: EdgeInsets.symmetric(horizontal: 10.0),
+        child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
                 Text(
-                  "Forgotten Password?",
+                  "Sign In",
                   style: TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.blueAccent,
+                    fontSize: 40.0,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Card(
-              elevation: 5.0,
-              child: Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
+                SizedBox(
+                  height: 100.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
-                    _buildEmailTextField(),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    _buildPasswordTextField(),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            GestureDetector(
-                child: Container(
-                height: 50.0,
-                decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(25.0)),
-                child: Center(
-                  child: Text(
-                    "Sign In",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                
-              ),
-              onTap: (){ Navigator.of(context).pushReplacement(MaterialPageRoute(
-                        builder: (BuildContext context) => MainScreen()));},
-            ),
-            Divider(
-              height: 20.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Don't have an account?",
-                  style: TextStyle(
-                      color: Color(0xFFBDC2CB),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0),
-                ),
-                SizedBox(width: 10.0),
-                GestureDetector(
-                  onTap: () {
-                    
-                  },
-                  child: Text(
-                    "Sign up",
-                    style: TextStyle(
+                    Text(
+                      "Forgotten Password?",
+                      style: TextStyle(
+                        fontSize: 18.0,
                         color: Colors.blueAccent,
                         fontWeight: FontWeight.bold,
-                        fontSize: 18.0),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Card(
+                  elevation: 5.0,
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      children: <Widget>[
+                        _buildEmailTextField(),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        _buildPasswordTextField(),
+                      ],
+                    ),
                   ),
                 ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                GestureDetector(
+                    child: Container(
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(25.0)),
+                    child: Center(
+                      child: Text(
+                        "Sign In",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    
+                  ),
+                  onTap: (){ Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (BuildContext context) => MainScreen()));},
+                ),
+                Divider(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(
+                          color: Color(0xFFBDC2CB),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0),
+                    ),
+                    SizedBox(width: 10.0),
+                    GestureDetector(
+                      onTap: () {
+                        
+                      },
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(
+                            color: Colors.blueAccent,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0),
+                      ),
+                    ),
+                  ],
+                ),
               ],
+          ),
             ),
-          ],
         ),
       ),
     );
