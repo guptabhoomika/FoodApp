80cf947a7a3226be85e8718290964e5270650cb9
guptabhoomika <43852276+guptabhoomika@users.noreply.github.com>
2020-03-26 14:16:37 +0530

    update

--- food_app/lib/src/pages/signupPage.dart
+++ food_app/lib/src/pages/signupPage.dart
@@ -1,5 +1,9 @@
+
 import 'package:flutter/material.dart';
-import '../pages/signInPage.dart';
+import 'package:food_app/src/pages/signInPage.dart';
+
+import 'package:food_app/src/screens/mainScreen.dart';
+
 
 class SignUpPage extends StatefulWidget {
   @override
@@ -8,7 +12,7 @@ class SignUpPage extends StatefulWidget {
 
 class _SignUpPageState extends State<SignUpPage> {
   bool _toggleVisibility = true;
-  bool _toggleConfirmVisibility = true;
+  bool toggleVisibility = true;
 
   Widget _buildEmailTextField() {
     return TextFormField(
@@ -21,8 +25,7 @@ class _SignUpPageState extends State<SignUpPage> {
       ),
     );
   }
-
-  Widget _buildUsernameTextField() {
+    Widget _buildUserNameTextField() {
     return TextFormField(
       decoration: InputDecoration(
         hintText: "Username",
@@ -56,8 +59,7 @@ class _SignUpPageState extends State<SignUpPage> {
       obscureText: _toggleVisibility,
     );
   }
-
-  Widget _buildConfirmPasswordTextField() {
+    Widget _buildConfirmPasswordTextField() {
     return TextFormField(
       decoration: InputDecoration(
         hintText: "Confirm Password",
@@ -68,110 +70,100 @@ class _SignUpPageState extends State<SignUpPage> {
         suffixIcon: IconButton(
           onPressed: () {
             setState(() {
-              _toggleConfirmVisibility = !_toggleConfirmVisibility;
+              toggleVisibility = !toggleVisibility;
             });
           },
-          icon: _toggleConfirmVisibility
+          icon: toggleVisibility
               ? Icon(Icons.visibility_off)
               : Icon(Icons.visibility),
         ),
       ),
-      obscureText: _toggleConfirmVisibility,
+      obscureText: toggleVisibility,
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Sign Up",
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 50.0,
-            ),
-            Card(
-              elevation: 5.0,
-              child: Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  children: <Widget>[
-                    _buildUsernameTextField(),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    _buildEmailTextField(),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    _buildPasswordTextField(),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    _buildConfirmPasswordTextField(),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Container(
-              height: 50.0,
-              decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(25.0)),
-              child: Center(
-                child: Text(
+      body: Center(
+        //padding: EdgeInsets.symmetric(horizontal: 10.0),
+        child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Text(
                   "Sign Up",
                   style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,
+                    fontSize: 40.0,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ),
-            Divider(
-              height: 20.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Already have an account?",
-                  style: TextStyle(
-                      color: Color(0xFFBDC2CB),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0),
+                SizedBox(
+                  height: 100.0,
                 ),
-                SizedBox(width: 10.0),
-                GestureDetector(
-                  onTap: (){
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) => SignInPage()));
-                  },
-                  child: Text(
-                    "Sign In",
-                    style: TextStyle(
-                        color: Colors.blueAccent,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0),
+               
+                
+                Card(
+                  elevation: 5.0,
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      children: <Widget>[
+                        _buildEmailTextField(),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        _buildUserNameTextField(),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+
+                        _buildPasswordTextField(),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        _buildConfirmPasswordTextField()
+                      ],
+                    ),
                   ),
                 ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                GestureDetector(
+                    child: Container(
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(25.0)),
+                    child: Center(
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    
+                  ),
+                  onTap: (){ Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (BuildContext context) => SignInPage()));},
+                ),
+                Divider(
+                  height: 20.0,
+                ),
+               
+               
               ],
+          ),
             ),
-          ],
         ),
       ),
     );
   }
 }
+
